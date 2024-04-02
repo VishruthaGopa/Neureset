1fa6ad6c727c91ba0f09482665edff92bad3b102
CaitlinWardle <71661061+CaitlinWardle@users.noreply.github.com>
2024-04-02 14:24:52 -0400

    Update neuresetdevice.cpp

--- neuresetdevice.cpp
+++ neuresetdevice.cpp
@@ -1,42 +1,72 @@
 #include "neuresetdevice.h"
 
-NeuresetDevice::NeuresetDevice(EEGHeadset* headset, QObject* parent) : QObject(parent), sessionInProgress(false) {
- sessionTimer = new Counter();
+NeuresetDevice::NeuresetDevice(EEGHeadset* headset, QObject* parent) : QObject(parent), sessionInProgress(false)
+,stage1(0),stage2(0),stage3(0),stage4(0){
+ sessionTimer = new QTimer(this);
+ QObject::connect(sessionTimer, &QTimer::timeout, this, &NeuresetDevice::treatNextHandler);
  sessionLog = new SessionLog();
  eegHeadset=headset;
  watcher = new QFutureWatcher<QPair<Electrode*, double>>(this);
  QObject::connect(eegHeadset,&EEGHeadset::newSession,this,&NeuresetDevice::gotNewSession);
- QObject::connect(sessionTimer, &Counter::sessionStarted, this, &NeuresetDevice::startSession);
- QObject::connect(sessionTimer, &Counter::sessionPaused, this, &NeuresetDevice::pauseSession);
- QObject::connect(sessionTimer, &Counter::sessionResumed, this, &NeuresetDevice::resumeSession);
- QObject::connect(sessionTimer, &Counter::sessionEnded, this, &NeuresetDevice::endSession);
-
 }
 
 void NeuresetDevice::calculateOverallBaseline() {
     eegHeadset->startMeasurement();
 }
 
+void NeuresetDevice::stopTreatment() {
+    sessionTimer->stop();
+}
 
 void NeuresetDevice::gotNewSession(Session* session){
     sessionLog->addSession(session);
 }
 
-void NeuresetDevice::startSession(){
-    if(!sessionInProgress){
-        sessionInProgress=true;
-        //handle start
-        calculateOverallBaseline();
-    }
-    else{
-        qInfo("Session already in Progress");
+void NeuresetDevice::startSession() {
+   if (!sessionInProgress) {
+       sessionInProgress = true;
+       //handle start
+       calculateOverallBaseline();
+       sessionTimer->start(50);
+   } else {
+       qInfo("Session already in Progress");
+   }
+}
+
+void NeuresetDevice::treatNextHandler() {
+    if (sessionInProgress) {
+        if (stage1 < 21) {
+            eegHeadset->treatNext(5.0);
+            stage1++;
+        }
+        else if (stage2 < 21) {
+            eegHeadset->treatNext(10.0);
+            stage2++;
+        }
+        else if (stage3 < 21) {
+            eegHeadset->treatNext(15.0);
+            stage3++;
+        }
+        else if (stage4 < 21) {
+            eegHeadset->treatNext(20.0);
+            stage4++;
+        }
+        else {
+            // All treatments completed, end the session
+            endSession();
+            return;
+        }
     }
 }
 
 void NeuresetDevice::endSession(){
     if(sessionInProgress){
         sessionInProgress=false;
-        //handle start
+        stage1=0;
+        stage2=0;
+        stage3=0;
+        stage4=0;
+        stopTreatment();
     }
     else{
         qInfo("No Session in Progress");
@@ -45,7 +75,7 @@ void NeuresetDevice::endSession(){
 
 void NeuresetDevice::pauseSession(){
     if(sessionInProgress){
-       //handle pause
+       stopTreatment();
     }
     else{
         qInfo("No session in progress");
