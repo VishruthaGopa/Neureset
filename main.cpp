a55c1ea4c82ee2f42f18ee1972c02f8c3b774969
CaitlinWardle <71661061+CaitlinWardle@users.noreply.github.com>
2024-04-03 19:48:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include "mainwindow.h"
-#include "neuresetdevice.h"
-#include "eegheadset.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -8,8 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    EEGHeadset *eegheadset=new EEGHeadset();
-    NeuresetDevice neureset(eegheadset);
 
     return a.exec();
 }
